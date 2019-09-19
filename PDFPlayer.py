8918bc419dc3fd55b226791ad2aed0825b80cabe
itzlalit143 <33309605+itzlalit143@users.noreply.github.com>
2019-09-19 21:28:36 +0530

    Update PDFPlayer.py

--- PDFPlayer.py
+++ PDFPlayer.py
@@ -1,19 +1,19 @@
 import pyttsx3
 import engineio
 import PyPDF2
-
+#tere kaam ki cheej nhi h####
 engineio = pyttsx3.init()
 voices = engineio.getProperty('voices')
 engineio.setProperty('rate', 130)
 engineio.setProperty('voice',voices[0].id)
-
-def speak(text):
+#############################
+def speak(text): #Function to speak text
     engineio.say(text)
     engineio.runAndWait()
 
-pdfFileObj = open(r'C:\Users\HP\Desktop\InterProcessComm.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-print(pdfReader.numPages)
-pageObj = pdfReader.getPage(5)
+pdfFileObj = open(r'C:\Users\HP\Desktop\InterProcessComm.pdf', 'rb') #Creating file object
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj) #file reading object
+print(pdfReader.numPages) #fetching number of pages
+pageObj = pdfReader.getPage(5) #creating object per page
 print(pageObj.extractText())
 speak(pageObj.extractText())
